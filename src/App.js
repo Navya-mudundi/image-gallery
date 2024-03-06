83c1bcce2cb78360f3ae6db174a3ca074a5292d9
navya <navyadev.33@gmail.com>
2024-03-06 12:29:20 +0530

    adding gallery code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,52 @@
-import React, { useState } from 'react'
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+import Gallery from './Gallery';
 
+
+const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 const App = () => {
-  const [city,setCity]=useState("");
-  const[result,setResult]=useState("");
-  const changeHandler= e =>{
-    setCity(e.target.value)
+  const [data,setData] = useState([]);
+  const [search,setSearch] = useState("");
+  useEffect(()=>{
+    },[])
+  const changeHandler = e =>{
+    setSearch(e.target.value);
   }
-  const submitHandler= e=>{
+  const submitHandler = e =>{
     e.preventDefault();
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then
-    (response=>response.json())
-    .then (data=>{
-      const kelvin=data.main.temp;
-      const celcius=kelvin-273.5;
-      setResult("Temperature at "+city+ " is"+"\n"+Math.round(celcius)+"°C");
-      
-    }
-    ).catch(error=>console.log("error"))
-    setCity("")
-      
-    };
-   
-    return (
+    axios
+    .get(
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
+    )
+    .then(response => {
+      setData(response.data.photos.photo)
+    })
+    .catch(error => {
+      console.log(
+        "Encountered an error with fetching and parsing data",
+        error
+      );
+  })
+  }
+  return (
     <div>
       <center>
-    <div className='card'>
-      <div className='card-body'>
-        <div className='card-title'>
-        <h1>weather Application</h1>
+        <h2>Gallery Snapshot</h2><br></br>
         <form onSubmit={submitHandler}>
-          <input type="text" name="city" value={city} onChange={changeHandler}/> &nbsp;&nbsp;
-          <input type="submit" value="search"/>
+          <input size="30" type="text" onChange={changeHandler} value={search}/><br /><br />
+          <input type="submit" name="Search" />
         </form>
-        </div>
-         <h1>{result}</h1>
-      </div>
-      
-    </div>
-    </center>
+        <br />
+        {data.length>=1?<Gallery data={data}/>:<h4>No Image Loaded</h4>}
+      </center>
     </div>
   )
 }
 
 export default App
+
+
+
+
 
 
